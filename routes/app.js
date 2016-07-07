f2ce8206ebf5e5517329b99b0979cae7c00af04f
Jacob "TKC" Deming <JacobDeming91@gmail.com>
2016-07-07 19:49:55 -0400

    Added Some More Routes

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+router.get('/profile',function(req,res,next){
+  res.render('index');
+});
+
+router.get('/projects',function(req,res,next){
+  res.render('index');
+})
+
 module.exports = router;
