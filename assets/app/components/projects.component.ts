91fd8c229eedd17305c45e097a2dfb258c2524d7
Jacob "TKC" Deming <JacobDeming91@gmail.com>
2016-07-07 18:29:37 -0400

    Added More Projects

--- assets/app/components/projects.component.ts
+++ assets/app/components/projects.component.ts
@@ -16,5 +16,13 @@ import {Project} from './projects/projects';
 
 export class ProjectsComponent{
   projects = [
-    new Project("Site-V-Site","http://site-v-site.herokuapp.com/")]
+    new Project("Site-V-Site","http://site-v-site.herokuapp.com/"),
+    new Project("Don't Get Me Started","https://dontgetmestarted.herokuapp.com/"),
+    new Project("Right or Wrong Quiz","https://rightorwrongquiz.herokuapp.com/"),
+    new Project("Harder Hangman","https://harder-hangman.herokuapp.com/"),
+    new Project("NYT Scrubber","https://nyt-scrubber.herokuapp.com"),
+    new Project("Jolly Cooperation","jolly-cooperation.herokuapp.com"),
+    new Project("Playlist of Power","https://playlistofpower.herokuapp.com/"),
+    new Project("Tavern Tussle","https://taverntussle.herokuapp.com/"),
+    new Project("Your Gif Collection","https://gifcollection.herokuapp.com/")]
 }
