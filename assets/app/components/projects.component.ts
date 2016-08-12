2e9e9e541ab197d90a5d25b8881088bd91391fd6
Jacob "TKC" Deming <JacobDeming91@gmail.com>
2016-08-12 17:07:45 -0400

    Fixed Jolly Cooperation Link

--- assets/app/components/projects.component.ts
+++ assets/app/components/projects.component.ts
@@ -27,7 +27,7 @@ export class ProjectsComponent{
     new Project("Right or Wrong Quiz","https://rightorwrongquiz.herokuapp.com/",""),
     new Project("Harder Hangman","https://harder-hangman.herokuapp.com/",""),
     new Project("NYT Scrubber","https://nyt-scrubber.herokuapp.com",""),
-    new Project("Jolly Cooperation","jolly-cooperation.herokuapp.com",""),
+    new Project("Jolly Cooperation","https://jolly-cooperation.herokuapp.com",""),
     new Project("Playlist of Power","https://playlistofpower.herokuapp.com/",""),
     new Project("Tavern Tussle","https://taverntussle.herokuapp.com/",""),
     new Project("Your Gif Collection","https://gifcollection.herokuapp.com/","")]
